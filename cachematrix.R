b6e5b94805c4f9e6ca6bda4efe127da24d848059
ArulSamuel <arajkuma@usc.edu>
2014-04-25 18:30:03 -0700

    Working Code for Caching Matrix Inverse

--- cachematrix.R
+++ cachematrix.R
@@ -1,6 +1,6 @@
+## Function Description: makeCacheMatrix()
 ## Function to create a special matrix
 makeCacheMatrix <- function(x = matrix()) {
-  #x=matrix(1:4,nrow=2,ncol=2)
   inv_matrix <- NULL
   
   ## Get and Set methods for the original and inverse matrix
@@ -16,25 +16,29 @@ makeCacheMatrix <- function(x = matrix()) {
   
 }
 
+## Function Description: cacheSolve()
 ## This function retrives the inverse of the matrix from the cache.
 ## If it is not in cache, it re-computes the inverse and returns the result.
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-  ##orig_matrix <- makeCacheMatrix()
+  ## Return a matrix that is the inverse of 'x'
+  inv_matrix <- NULL
   
+  ## Calling the fuction to set the value in the cache
+  x$set_inv()
+  
+  ## Getting the value from the cache
   inv_matrix <- x$get_inv()
   if(!is.null(inv_matrix)) {
-    message("Getting cached inverse")
+    message("Retrived the inverse Matrix from cache")
     return(inv_matrix)
   }
-  matrix <- x$get()
-  inv_matrix <- solve(matrix)
-  x$set_inv(inv_matrix)
+  
+  ## Could not get the inverse matrix from the cache
   inv_matrix
 }
 
 ## Function calls for testing
-
-x <- matrix(1:4,nrow=2,ncol=2)
-makeCacheMatrix(x)
-inverse <- cacheSolve(x)
+x <- matrix(2:5,nrow=2,ncol=2)
+mat <- makeCacheMatrix(x)
+inverse <- cacheSolve(mat)
+print(inverse)
